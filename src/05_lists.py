d14b3b885f5fbcb5a9779b8f20b0fe392ff9d521
trucane <trufashionista2013@gmail.com>
2019-10-01 11:06:05 -0500

    nore of the first assignment

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,25 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
-print(x)
+x.append(4)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x)
+for i in range(0, len(y)): x.append(y[i])
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
-print(x)
+x.remove(8)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
-print(x)
+x.insert(5,99)
 
 # Print the length of list x
 # YOUR CODE HERE 
+print(x)
+
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for i in range(0, len(x)): print(x[i] * 1000)
